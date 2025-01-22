f75bac36d5bee100c26b6eaf8000ac3f1a060975
unknown <allanulisses@hotmail.com>
2025-01-21 21:21:53 -0300

    model replaced

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/allanVvz/GoCRUD/src/config/logger"
 	"github.com/allanVvz/GoCRUD/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("Starting the application")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
